c16a01c23744cd7076a2e93b1f05bebf9ff5908e
mmark6 <mmukisa64@gmail.com>
2026-05-11 20:13:18 +0300

    Fix formatting in CriteriaWeightCard component

--- iles-frontend/src/features/evaluations/components/CriteriaWeightCard.jsx
+++ iles-frontend/src/features/evaluations/components/CriteriaWeightCard.jsx
@@ -1,3 +1,4 @@
+
 import Card from '../../../components/common/Card'
 export default function CriteriaWeightCard() {
   return <Card title="Default Weighting"><p>Technical Skills: 50%</p><p>Communication: 20%</p><p>Professionalism: 30%</p></Card>
