060fd8c99964edd35a4c8864ec9bbb91eeb2d2f3
mmark6 <mmukisa64@gmail.com>
2026-06-09 15:22:38 +0300

    Update settings.py

--- iles-backend/config/settings.py
+++ iles-backend/config/settings.py
@@ -19,9 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ubuyaf(*q#w05_d(uu(fb!q4-6s*dalg&!ia%sn7&^h)jnr-oo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["mukisa-api.tagooledavid.com",
+    "localhost",
+    "127.0.0.1"]
 
 
 # Application definition
